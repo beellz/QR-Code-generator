f67fab3253aa3b6f831d9ddc2a9d16dd6c6b622c
silvercry007 <78276470+silvercry007@users.noreply.github.com>
2023-01-25 12:58:38 +0530

    QR Code generator for URL's or Information.

---
+++ QR_generator.py
@@ -0,0 +1,6 @@
+import pyqrcode
+from PIL import Image
+url = input("Enter anything you want a QR code for : ")
+qr_code = pyqrcode.create(url)
+qr_code.png("QRCode.png", scale=5)
+Image.open("QRCode.png")
