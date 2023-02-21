8927b4d10a21363941ba6cf7ddbba7c3824ba38b
silvercry007 <shubhamrules007@gmail.com>
2023-02-21 22:52:20 +0530

    Url name is dynamic and storing multiple outputs

--- QR_generator.py
+++ QR_generator.py
@@ -2,6 +2,5 @@ import pyqrcode
 from PIL import Image
 url = input("Enter anything you want a QR code for : ")
 qr_code = pyqrcode.create(url)
-qr_code.png("QRCode.png", scale=5)
-Image.open("QRCode.png")
+qr_code.png((url + ".png"), scale=5)
 print('QR code has been Generated and saved on the local folder')
